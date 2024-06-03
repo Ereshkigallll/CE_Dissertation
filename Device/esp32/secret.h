5a7f065f0dffabb330b76f9cec7d9852602e9df9
zhangyutong <zhangyutong@sailyond.com>
2024-06-03 16:35:42 +0100

    mobile app create

---
+++ Device/esp32/secret.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID "2C93 Hyperoptic Fibre Broadband"
+#define SECRET_PASS "JYjKsjEn2JEQ"
+
+#define SECRET_MQTTUSER "student"
+#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
+
+
